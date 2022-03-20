228ef9279fcb8008b47fd1dcfa37537b9eff7dd8
Nikola Selic <selich.work@gmail.com>
2022-03-20 21:23:50 +0100

    fix: 🐛 fix href

--- components/navbar.js
+++ components/navbar.js
@@ -25,7 +25,7 @@ const LinkItem = ({ href, path, children }) => {
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
 
   return (
-    <NextLink href={href}>
+    <NextLink href={href} passHref>
       <Link
         p={2}
         bg={active ? 'glassTeal' : undefined}
