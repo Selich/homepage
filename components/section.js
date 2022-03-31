4f0952e7b42dcb92a145c85c82fa8d59bd8f6214
Nikola Selic <selich.work@gmail.com>
2022-03-31 21:51:39 +0200

    fix cards

--- components/section.js
+++ components/section.js
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion'
 import { chakra, shouldForwardProp } from '@chakra-ui/react'
-import { Container } from '@chakra-ui/react'
 
 const StyledDiv = chakra(motion.div, {
   shouldForwardProp: prop => {
@@ -12,26 +11,10 @@ const Section = ({ children, delay = 0 }) => (
   <StyledDiv
     initial={{ opacity: 0, y: 10 }}
     animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.8, delay }}
-    cursor={'pointer'}
+    transition={{ duration: 0.8, delay}}
     mb={6}
-    backdropBlur={10}
-    backgroundColor={'#7928ca36'}
-    height={300}
-    borderRadius={20}
-    boxShadow="0 1rem 1rem rgb(0 0 0 / 20%)"
   >
-    <Container
-      style={{
-        backgroundColor: 'white',
-        marginTop: "70%",
-        height: "110px",
-        borderBottomLeftRadius: "10px",
-        borderBottomRightRadius: "10px",
-      }}
-    >
-      {children}
-    </Container>
+    {children}
   </StyledDiv>
 )
 
--- pages/works.js
+++ pages/works.js
@@ -1,8 +1,14 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 // import thumb from '../public/image/works/'
 import Layout from '../components/layouts/article'
-import Section from '../components/section'
+import { motion } from 'framer-motion'
+import { chakra, shouldForwardProp } from '@chakra-ui/react'
 
+const StyledDiv = chakra(motion.div, {
+  shouldForwardProp: prop => {
+    return shouldForwardProp(prop) || prop === 'transition'
+  }
+})
 const Works = () => {
   return (
     <Layout>
@@ -25,5 +31,31 @@ const Works = () => {
     </Layout>
   )
 }
+const Section = ({ children, delay = 0 }) => (
+  <StyledDiv
+    initial={{ opacity: 0, y: 10 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.8, delay }}
+    cursor={'pointer'}
+    mb={6}
+    backdropBlur={10}
+    backgroundColor={'#7928ca36'}
+    height={300}
+    borderRadius={20}
+    boxShadow="0 1rem 1rem rgb(0 0 0 / 20%)"
+  >
+    <Container
+      style={{
+        backgroundColor: 'white',
+        marginTop: "70%",
+        height: "110px",
+        borderBottomLeftRadius: "10px",
+        borderBottomRightRadius: "10px",
+      }}
+    >
+      {children}
+    </Container>
+  </StyledDiv>
+)
 
 export default Works
