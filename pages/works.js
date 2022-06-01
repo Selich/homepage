1dc7c93c8a580f9d10bbc2de4acd7c90987b4354
Nikola Selic <ge58lir@mytum.de>
2022-06-01 13:43:36 +0200

    refactor works

--- pages/works.js
+++ pages/works.js
@@ -11,13 +11,21 @@ const StyledDiv = chakra(motion.div, {
   }
 })
 
-const items = [{ id: 0 }, { id: 1 }, { id: 2 }]
+const items = [{ id: 1 }, { id: 2 }, { id: 3 }]
 const Works = () => {
-  const [selectedItem, setSelectedItem] = useState(null)
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/Selich`)
+      .then(response => {
+        console.log(response.json())
+        setUser(response.json())
+      })
+  }, [])
 
   useEffect(() => {
     console.log(selectedItem)
-
   }, [selectedItem])
   return (
     <Layout>
@@ -27,95 +35,113 @@ const Works = () => {
         </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           {items.map(item => (
-            <Section item={item} setSelectedItem={setSelectedItem} key={item.id}>
+            <Section
+              item={item}
+              setSelectedItem={setSelectedItem}
+              key={item.id}
+            >
               {item.id}
             </Section>
           ))}
         </SimpleGrid>
         <AnimatePresence>
           {selectedItem && (
-        <SimpleGrid columns={[1, 1, 2]} gap={6}>
-            <SectionMain selectedItem={selectedItem} setSelectedItem={setSelectedItem}  layoutId={selectedItem.id}>
-              <motion.h5>test</motion.h5>
-              <Button onClick={() => setItem(null)} />
-            </SectionMain>
-        </SimpleGrid>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+              <SectionMain
+                selectedItem={selectedItem}
+                setSelectedItem={setSelectedItem}
+                layoutId={selectedItem.id}
+              >
+                <motion.h5>test</motion.h5>
+                <Button onClick={() => setItem(null)} />
+              </SectionMain>
+            </SimpleGrid>
           )}
         </AnimatePresence>
       </Container>
     </Layout>
   )
 }
-const SectionMain = ({ selectedItem, setSelectedItem, children, delay = 0 }) => 
-{
-  useEffect(() => {},[selectedItem])
-  return (
-  <StyledDiv
-    onClick={() => setSelectedItem(undefined)}
-    initial={{ opacity: 0, y: 10 }}
-    animate={{ y: 0, opacity: 1 }}
-  pos={'absolute'}
-  width={580}
-  marginRight={'20%'}
-  top={0}
-    transition={{ duration: 0.2, delay }}
-    cursor={'pointer'}
-    backdropBlur={10}
-    backgroundColor={'white'}
-    height={400}
-    borderRadius={20}
-    boxShadow="0 1rem 1rem rgb(0 0 0 / 20%)"
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
-  >
-    <Container
-      style={{
-        backgroundColor: '#7928ca36',
-        height: '310px',
-        borderTopLeftRadius: '10px',
-        borderTopRightRadius: '10px'
-      }}
-    >
-      {children}
-    </Container>
-  </StyledDiv>
-)
-}
-const Section = ({ selectedItem, item, setSelectedItem, children, delay = 0 }) => {
 
+// TODO: Add as sticky
+// TODO: Black transparent background 
+const SectionMain = ({
+  selectedItem,
+  setSelectedItem,
+  children,
+  delay = 0
+}) => {
+  useEffect(() => {}, [selectedItem])
+  return (
+    <StyledDiv
+      onClick={() => setSelectedItem(undefined)}
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ y: 0, opacity: 1 }}
+      pos={'absolute'}
+      width={580}
+      marginRight={'20%'}
+      top={0}
+      transition={{ duration: 0.2, delay }}
+      cursor={'pointer'}
+      backdropBlur={10}
+      backgroundColor={'white'}
+      height={400}
+      borderRadius={20}
+      boxShadow="0 1rem 1rem rgb(0 0 0 / 20%)"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <Container
+        style={{
+          backgroundColor: '#7928ca36',
+          height: '310px',
+          borderTopLeftRadius: '10px',
+          borderTopRightRadius: '10px'
+        }}
+      >
+        {children}
+      </Container>
+    </StyledDiv>
+  )
+}
+const Section = ({
+  selectedItem,
+  item,
+  setSelectedItem,
+  children,
+  delay = 0
+}) => {
   useEffect(() => {
     console.log(selectedItem)
-
-  },[selectedItem])
+  }, [selectedItem])
   return (
-
-  <StyledDiv
-    onClick={() => setSelectedItem(item.id)}
-    initial={{ opacity: 0, y: 10 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.2, delay }}
-    cursor={'pointer'}
-    mb={6}
-    backdropBlur={(selectedItem !== undefined) ? 40: 10}
-    backgroundColor={(selectedItem !== undefined) ? 'gray' : 'white'}
-    height={300}
-    borderRadius={20}
-    boxShadow="0 1rem 1rem rgb(0 0 0 / 20%)"
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
-  >
-    <Container
-      style={{
-        backgroundColor: '#7928ca36',
-        height: '210px',
-        borderTopLeftRadius: '10px',
-        borderTopRightRadius: '10px'
-      }}
+    <StyledDiv
+      onClick={() => setSelectedItem(item.id)}
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.2, delay }}
+      cursor={'pointer'}
+      mb={6}
+      backdropBlur={selectedItem !== undefined ? 40 : 10}
+      backgroundColor={selectedItem !== undefined ? 'gray' : 'white'}
+      height={300}
+      borderRadius={20}
+      boxShadow="0 1rem 1rem rgb(0 0 0 / 20%)"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
     >
-      {children}
-    </Container>
-  </StyledDiv>
-)
+      <Container
+        style={{
+          backgroundColor: '#7928ca36',
+          height: '210px',
+          borderTopLeftRadius: '10px',
+          borderTopRightRadius: '10px'
+        }}
+      >
+        {children}
+      </Container>
+    </StyledDiv>
+  )
 }
 
 export default Works
